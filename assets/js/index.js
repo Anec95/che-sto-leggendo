33827c93c78adf809a7da9e53b4f938509b9db42
Anec95 <113508749+Anec95@users.noreply.github.com>
2022-12-13 14:59:07 +0100

    sostituzione textarea con input text

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 let closeButton = document.getElementsByClassName('fa-times')[0];
-let resultContainer = document.getElementsByClassName('result-container');
+let resultContainer = document.querySelectorAll('.result-container');
 let infoContainer = document.getElementsByClassName('info-container')[0];
 let researchPool = document.getElementsByClassName('research-results')[0];
 let titlePanelInfo = document.getElementsByClassName('over-info')[0];
@@ -23,6 +23,6 @@ researchPool.onclick = function(event) {
     let numberDiv = event.target.index; 
     console.log(numberDiv)
     infoContainer.style.display = 'block';
-    let titleResult = event.target.getElementsByClassName('title-result');
+    let titleResult = event.target.querySelector('.title-result');
     titlePanelInfo.innerText = titleResult[numberDiv].textContent;
 }
